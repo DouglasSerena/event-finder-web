3ca3f8a28d5ba1b7cad2131f75d17e1dfad33de1
DouglasSerena <douglas222serena@rede.ulbra.br>
2021-10-03 09:50:43 -0300

    fix: Ajustado para usar api vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   PRODUCTION: true,
-  URL_APP: 'https://finderevent-web.herokuapp.com',
-  URL_API: 'https://finderevent-api.herokuapp.com/api',
+  URL_APP: 'https://event-finder-web.vercel.app',
+  URL_API: 'https://event-finder-api.vercel.app/api',
   TOKEN_MAPBOX:
     'pk.eyJ1IjoiZG91Z2xhc3NlcmVuYSIsImEiOiJja2VrMmR6bXMxc3czMnltejIzbXh5eHIwIn0.uUxqAx39JExJ__-KxxhEyQ',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   PRODUCTION: false,
   URL_API: 'http://localhost:3000/api',
-  URL_APP: 'https://finderevent-web.herokuapp.com',
-  // URL_API: 'https://finderevent-api.herokuapp.com/api',
+  URL_APP: 'https://event-finder-web.vercel.app',
+  // URL_API: 'https://event-finder-api.vercel.app/api',
   TOKEN_MAPBOX:
     'pk.eyJ1IjoiZG91Z2xhc3NlcmVuYSIsImEiOiJja2VrMmR6bXMxc3czMnltejIzbXh5eHIwIn0.uUxqAx39JExJ__-KxxhEyQ',
 };
