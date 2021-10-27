ea462159eed91ef8e10ba2a273edcfae945163c8
DouglasSerena <douglas222serena@rede.ulbra.br>
2021-10-27 19:17:29 -0300

    fix: Ajustado modal de busca

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   PRODUCTION: false,
   URL_APP: 'https://event-finder-web.herokuapp.com',
-  URL_API: 'http://localhost:3000/api',
-  // URL_API: 'https://event-finder-api.herokuapp.com/api',
+  // URL_API: 'http://localhost:3000/api',
+  URL_API: 'https://event-finder-api.herokuapp.com/api',
   TOKEN_MAPBOX:
     'pk.eyJ1IjoiZG91Z2xhc3NlcmVuYSIsImEiOiJja2VrMmR6bXMxc3czMnltejIzbXh5eHIwIn0.uUxqAx39JExJ__-KxxhEyQ',
 };
